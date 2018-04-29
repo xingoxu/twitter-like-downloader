cb89e1911b0ba4f4e5f85b9905dae4e3238c1731
xingoxu <xingoxu@gmail.com>
2018-04-29 21:34:00 +0900

    update readme

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json({"title": "Welcome to RESTAPI service!"});
+  res.json({"title": "Twitter Bot By xingoxu!"});
 });
 
 module.exports = router;
